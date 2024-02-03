d967392ff5c98f48bf8eaf6ed305373b9a014de7
aparnab4 <manishabalel@gmail.com>
2024-02-03 01:54:39 -0800

    "progress"

--- src/test/java/com/solvd/gadgets/CustomersTestCases.java
+++ src/test/java/com/solvd/gadgets/CustomersTestCases.java
@@ -1,80 +1,93 @@
 package com.solvd.gadgets;
 
-import com.solvd.gadgets.bin.Customer;
-import com.solvd.gadgets.dao.daoInterfaces.CustomerDAO;
-import com.solvd.gadgets.dao.impl.myBatis.CustomerDAOImpl;
-import com.solvd.gadgets.service.jdbc.CustomerServiceImpl;
-import com.solvd.gadgets.service.serviceInterfaces.CustomerService;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
-
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class CustomersTestCases {
     private static final Logger LOGGER = Logger.getLogger(CustomersTestCases.class.getName());
- SoftAssert softAssert = new SoftAssert();
-    CustomerDAO customerDao = new CustomerDAOImpl();
-    CustomerService customerService = new CustomerServiceImpl(customerDao);
+
+
+
     @Test (priority = 1)
     public void CreateNewCustomerTest() {
-         customerService.insertCustomer("Naia", "Bisht", "naia.bisht@example.com", 2727272727l);
-        Optional<Customer> customer = customerService.getCustomerById(34);
-        assertEquals(customer.get().getFirstName(), "Naia");
-        assertEquals(customer.get().getLastName(), "Bisht");
-        assertEquals(customer.get().getEmail(), "naia.bisht@example.com");
-        assertEquals(customer.get().getPhone(), 2727272727l);
+        String firstName = null, lastName = "Doe", email = "john.doe@example.com";
+        long phone = 1234567890L;
 
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertNotNull(firstName, "First name is null");
+        softAssert.assertNotNull(lastName, "Last name is null");
+        softAssert.assertNotNull(email, "Email is null");
+        softAssert.assertNotNull(phone, "Phone number is null");
+        softAssert.assertAll();
     }
 
     @Test (priority = 2)
-    public void GetCustomerByIDTest() {
-        customerService.insertCustomer("John", "Doe", "john.doe@example.com", 1234567890);
-        Optional<Customer> customer = customerService.getCustomerById(23);
-        assertTrue(customer.isPresent());
-        assertEquals(customer.get().getFirstName(), "John");
-        assertEquals(customer.get().getLastName(), "Doe");
-        assertEquals(customer.get().getEmail(), "john.doe@example.com");
-        assertEquals(customer.get().getPhone(), 1234567890);
-    }
+    public void NumbersInFirstNameTest() {
+            String firstName = "John111";
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertTrue(firstName.matches(".*\\d.*"), "Numbers should not be allowed in the first name field");
+            softAssert.assertAll();
+        }
 
     @Test (priority = 3)
+    public void NumbersInLastNameTest( ) {
+            String lastName = "Doe123";
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertTrue(lastName.matches(".*\\d.*"), "Numbers should not be allowed in the last name field");
+            softAssert.assertAll();
+        }
+
+
+    @Test (priority = 4)
+    public void InvalidEmailFormatTest() {
+        String email = "invalid-email";
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(email.contains( "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +  "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"), "invalid email format");
+        softAssert.assertAll();
+    }
+    @Test (priority = 5)
+    public void InvalidPhoneTest(String firstName, String lastName, String Email, long Phone) {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(String.valueOf(Phone).matches("^\\d{10}$"), "invalid phone number: " + Phone);
+        softAssert.assertAll();
+    }
+
+
+    @Test (priority = 6)
+    public void GetCustomerByIDTest() {
+        String firstName = "Aaa", lastName = "Bbb", email = "aaa_BBB@example.com";
+        long phone = 1234567891;
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(firstName, "Aaa", "First name mismatch");
+        softAssert.assertEquals(lastName, "Bbb", "Last name mismatch");
+        softAssert.assertEquals(email, "aaa_BBB@example.com", "Email mismatch");
+        softAssert.assertEquals(phone, 1234567891, "Phone number mismatch");
+        softAssert.assertAll();
+    }
+
+    @Test (priority = 7)
     public void UpdateCustomerTest() {
 
     }
 
-    @Test (priority = 4)
+   @Test (priority = 8)
     public void DeleteCustomerTest() {
 
     }
 
-    @Test (priority = 5)
+    @Test (priority = 9)
     public void GetAllCustomersTest() {
 
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void NumbersInFirstNameTest() {
-        softAssert.fail("first name contain numbers");
 
-        customerService.insertCustomer("John123", "Doe", "john.doe@example.com", 1234567890);
-    }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void NumbersInLastNameTest() {
-        customerService.insertCustomer("John", "Doe123", "john.doe@example.com", 1234567890);
-        softAssert.fail("last name contain numbers");
-    }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void InvalidEmailFormatTest() {
-        customerService.insertCustomer("John", "Doe", "invalid-email", 1234567890);
-        softAssert.fail("invalid email");
-        softAssert.assertAll();
-    }
+
 
 }
---
+++ src/test/java/com/solvd/gadgets/listeners/TestNGListener.java
@@ -0,0 +1,27 @@
+package com.solvd.gadgets.listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestNGListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ITestListener.super.onTestStart(result);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ITestListener.super.onTestSuccess(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ITestListener.super.onTestFailure(result);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+}
